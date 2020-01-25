a0969bd91e79cd4775694f22f3ce0012e63cc3f3
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2020-01-24 19:27:37 -0500

    update nuget

--- .github/actions/pack-solution/entrypoint-script.sh
+++ .github/actions/pack-solution/entrypoint-script.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-alias nuget="mono /usr/local/bin/nuget.exe"
-
-sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-targetNugetExe = ".\nuget.exe"
-nuget install Microsoft.CrmSdk.CoreTools
-
 echo  "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
